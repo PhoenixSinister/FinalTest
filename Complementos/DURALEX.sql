cc7757d6f7ec7be27c512c420f8f3a5dbf8d52f0
PPR <>
2017-06-28 20:19:26 -0400

    ACTUALIZACION SQL

--- Complementos/DURALEX.sql
+++ Complementos/DURALEX.sql
@@ -1,115 +1,35 @@
-/*
-SQLyog Ultimate v11.11 (64 bit)
-MySQL - 5.5.5-10.1.21-MariaDB : Database - duralex
-*********************************************************************
-*/
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`duralex` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
+/*ACTUALIZACION */
 USE `duralex`;
 
-/*Table structure for table `abogado` */
 
-DROP TABLE IF EXISTS `abogado`;
+CREATE TABLE ROLES(
+ID INT PRIMARY KEY NOT NULL,
+DESCRIPCION VARCHAR(100)
+);
 
-CREATE TABLE `abogado` (
-  `rut` varchar(12) NOT NULL,
-  `nombre` varchar(50) DEFAULT NULL,
-  `fecha_cont` date DEFAULT NULL,
-  `especialidad` int(3) DEFAULT NULL,
-  `valor_hora` int(15) DEFAULT NULL,
-  PRIMARY KEY (`rut`),
-  KEY `especialidad` (`especialidad`),
-  /*CONSTRAINT `abogado_ibfk_1` FOREIGN KEY (`especialidad`) REFERENCES `especialidad` (`cod_espe`)*/
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+insert into ROLES (ID, DESCRIPCION) values (0 , 'Sin Perfil');
+insert into ROLES (ID, DESCRIPCION) values (1 , 'Gerente');
+insert into ROLES (ID, DESCRIPCION) values (2 , 'Administrador');
+insert into ROLES (ID, DESCRIPCION) values (3 , 'Secretaria');
+insert into ROLES (ID, DESCRIPCION) values (4 , 'Cliente');
 
-/*Table structure for table `atencion` */
 
-DROP TABLE IF EXISTS `atencion`;
 
-CREATE TABLE `atencion` (
-  `numero_ate` int(15) NOT NULL AUTO_INCREMENT,
-  `fecha_hora` date DEFAULT NULL,
-  `cliente_rut` varchar(12) DEFAULT NULL,
-  `abogado_rut` varchar(12) DEFAULT NULL,
-  `estado_cod` int(3) DEFAULT NULL,
-  `valor_ate` int(15) DEFAULT NULL,
-  PRIMARY KEY (`numero_ate`),
-  KEY `cliente_rut` (`cliente_rut`),
-  KEY `abogado_rut` (`abogado_rut`),
-  KEY `estado_cod` (`estado_cod`),
-  CONSTRAINT `atencion_ibfk_1` FOREIGN KEY (`cliente_rut`) REFERENCES `cliente` (`rut`),
-  CONSTRAINT `atencion_ibfk_2` FOREIGN KEY (`abogado_rut`) REFERENCES `abogado` (`rut`),
-  CONSTRAINT `atencion_ibfk_3` FOREIGN KEY (`estado_cod`) REFERENCES `estado` (`cod_estado`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE USUARIO(
+ ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT
+,RUT VARCHAR(100) NOT NULL
+,NOMBRE VARCHAR(100) NOT NULL
+,FECHA VARCHAR(100) DEFAULT NULL
+,TIPO_PERSONA VARCHAR(100) NOT NULL
+,DIRECCION VARCHAR(100) NOT NULL
+,CELULAR VARCHAR(100) NOT NULL
+,CONTRASENA VARCHAR(100) NOT NULL
+,ID_PADRE VARCHAR(100)
+);
 
-/*Table structure for table `cliente` */
 
-DROP TABLE IF EXISTS `cliente`;
+insert into USUARIO (ID, RUT, NOMBRE, FECHA, TIPO_PERSONA, DIRECCION, CELULAR,CONTRASENA,ID_PADRE) VALUES (1,'1-9','Hugo','27-06-2017','Natural','personal@rescue.cl','9999999','1234',null);
+insert into USUARIO (ID, RUT, NOMBRE, FECHA, TIPO_PERSONA, DIRECCION, CELULAR,CONTRASENA,ID_PADRE) VALUES (2,'2-9','Pedro','27-06-2017','Natural','padre@rescue.cl','9999999','1234',null);
+insert into USUARIO (ID, RUT, NOMBRE, FECHA, TIPO_PERSONA, DIRECCION, CELULAR,CONTRASENA,ID_PADRE) VALUES (3,'3-9','Hijo','27-06-2017','Juridica','hijo@rescue.cl','9999999','1234',2);
 
-CREATE TABLE `cliente` (
-  `rut` varchar(12) NOT NULL,
-  `nombre` varchar(50) DEFAULT NULL,
-  `fecha_inc` date DEFAULT NULL,
-  `tipo_persona` char(1) DEFAULT NULL,
-  `direccion` varchar(30) DEFAULT NULL,
-  `telefono` int(12) DEFAULT NULL,
-  PRIMARY KEY (`rut`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Table structure for table `especialidad` */
-
-DROP TABLE IF EXISTS `especialidad`;
-
-CREATE TABLE `especialidad` (
-  `cod_espe` int(3) NOT NULL,
-  `nom_espe` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`cod_espe`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Table structure for table `estado` */
-
-DROP TABLE IF EXISTS `estado`;
-
-CREATE TABLE `estado` (
-  `cod_estado` int(3) NOT NULL,
-  `nombre_estado` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`cod_estado`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Table structure for table `perfil` */
-
-DROP TABLE IF EXISTS `perfil`;
-
-CREATE TABLE `perfil` (
-  `cod_perfil` int(3) NOT NULL,
-  `nombre_perfil` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`cod_perfil`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*Table structure for table `usuario` */
-
-DROP TABLE IF EXISTS `usuario`;
-
-CREATE TABLE `usuario` (
-  `rut` varchar(12) NOT NULL,
-  `nombre_completo` varchar(50) DEFAULT NULL,
-  `perfil_cod` int(3) DEFAULT NULL,
-  `contraseña` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`rut`),
-  KEY `perfil_cod` (`perfil_cod`),
-  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`perfil_cod`) REFERENCES `perfil` (`cod_perfil`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
